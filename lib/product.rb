92d944aaf4f6198a5e53f3b194e082c9d93662dc
possession4 <himebrittny@hotmail.com>
2016-03-23 22:13:42 -0400

    Added method that checks for duplicate products

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,24 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+     if !@@products.any? { |product| product.title == @title }
+     	@@products << self
+	else 
+		raise DuplicateProductError, "'#{@title}' already exists."
+	end
   end
 end
