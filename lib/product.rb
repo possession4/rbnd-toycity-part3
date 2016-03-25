b59b79e38bb3a01f5537b580894a07f5399a494f
possession4 <himebrittny@hotmail.com>
2016-03-25 00:04:42 -0400

    Added methods to find items by title, display product attributes, and check to see if items are in stock.

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,29 @@
 class Product
-  attr_reader :title
+  attr_accessor :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title)
+     @@products.find {|product| product.title == title} 
+  end
+
+  def in_stock?
+     self.stock > 0
+  end
+
+  def self.in_stock
+     @@products.select {|product| product.in_stock? == true} 
   end
 
   private
